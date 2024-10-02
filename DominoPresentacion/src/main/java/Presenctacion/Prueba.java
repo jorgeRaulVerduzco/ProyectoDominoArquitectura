df31de7125580700f452b5a6721f4307b18da646
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-01 18:55:26 -0700

    commit de prueba

--- DominoPresentacion/src/main/java/Presenctacion/Prueba.java
+++ DominoPresentacion/src/main/java/Presenctacion/Prueba.java
@@ -2,17 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Negocio;
-
-import Dominio.Ficha;
-import Dominio.Pozo;
-import java.util.List;
+package Presenctacion;
 
 /**
  *
  * @author INEGI
  */
-public class ServicioPozo {
-
-  
+public class Prueba {
+    
 }
