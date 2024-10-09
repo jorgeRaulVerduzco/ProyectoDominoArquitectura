27ba9207a06ed9225618653f66dfd602d881183b
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-09 02:52:46 -0700

    DTO

--- DominoNegocio/src/main/java/DTOs/FichaDTO.java
+++ DominoNegocio/src/main/java/DTOs/FichaDTO.java
@@ -9,5 +9,50 @@ package DTOs;
  * @author INEGI
  */
 public class FichaDTO {
-    
+
+    private int espacio1;
+    private int espacio2;
+    private boolean colocada = false;
+
+    public FichaDTO() {
+    }
+
+    public boolean isColocada() {
+        return colocada;
+    }
+
+    public void setColocada(boolean colocada) {
+        this.colocada = colocada;
+    }
+
+    public FichaDTO(int espacio1, int espacio2) {
+        this.espacio1 = espacio1;
+        this.espacio2 = espacio2;
+    }
+
+    public int getEspacio1() {
+        return espacio1;
+    }
+
+    public void setEspacio1(int espacio1) {
+        this.espacio1 = espacio1;
+    }
+
+    public int getEspacio2() {
+        return espacio2;
+    }
+
+    public void setEspacio2(int espacio2) {
+        this.espacio2 = espacio2;
+    }
+
+    public boolean esMula() {
+        return espacio1 == espacio2;
+    }
+
+    @Override
+    public String toString() {
+        return "Ficha{" + "espacio1=" + espacio1 + ", espacio2=" + espacio2 + '}';
+    }
+
 }
