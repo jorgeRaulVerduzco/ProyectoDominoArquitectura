09f80dd73f4e1ae30e14b98d86fab09ea00b287d
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-08 23:01:15 -0700

    guardar Fichas en el pozo

--- DominoNegocio/src/main/java/Negocio/ServicioPozo.java
+++ DominoNegocio/src/main/java/Negocio/ServicioPozo.java
@@ -6,13 +6,22 @@ package Negocio;
 
 import Dominio.Ficha;
 import Dominio.Pozo;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author INEGI
  */
 public class ServicioPozo {
+
+    private Pozo pozo;
+    private Random random = new Random();
+
+    public ServicioPozo() {
+        pozo = new Pozo();
+    }
 
     public Ficha tomarFichaDelPozo(Pozo pozo, int indice) {
         if (pozo.getFichasPozo().isEmpty()) {
@@ -26,8 +35,30 @@ public class ServicioPozo {
         return pozo.getFichasPozo().remove(indice);
     }
 
-    public void agregarFichasAlPozo(Pozo pozo, List<Ficha> fichas) {
-        pozo.getFichasPozo().addAll(fichas);
+    public void guardarFichasPozo() {
+        List<Ficha> fichas = new ArrayList<>();
+        for (int i = 0; i < 28; i++) { 
+            int ladoIzquierdo;
+            int ladoDerecho;
+
+            if (fichas.isEmpty()) {
+                ladoIzquierdo = random.nextInt(7); 
+                ladoDerecho = random.nextInt(7);   
+            } else {
+                Ficha ultimaFicha = fichas.get(fichas.size() - 1);  
+                ladoIzquierdo = ultimaFicha.getEspacio2();  
+                ladoDerecho = random.nextInt(7);  
+            }
+
+          
+            if (ladoIzquierdo >= 0 && ladoIzquierdo <= 6 && ladoDerecho >= 0 && ladoDerecho <= 6) {
+                Ficha nuevaFicha = new Ficha(ladoIzquierdo, ladoDerecho);
+                fichas.add(nuevaFicha);  // Agregar ficha generada
+            } else {
+                i--;  
+            }
+        }
+        pozo.setFichasPozo(fichas);  // Asigna las fichas generadas al pozo
     }
 
 }
