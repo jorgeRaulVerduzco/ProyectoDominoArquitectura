16fb138e55fa45d7ae9a574ce746315a776bbcfc
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-09 02:57:35 -0700

    Minimos cambios

--- DominoNegocio/src/main/java/DTOs/PozoDTO.java
+++ DominoNegocio/src/main/java/DTOs/PozoDTO.java
@@ -4,10 +4,35 @@
  */
 package DTOs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author INEGI
  */
 public class PozoDTO {
-    
+      private List<FichaDTO> fichasPozo;
+
+    public PozoDTO() {
+        fichasPozo = new ArrayList<>();
+    }
+
+    public PozoDTO(List<FichaDTO> fichasPozo) {
+        this.fichasPozo = fichasPozo;
+    }
+
+    public List<FichaDTO> getFichasPozo() {
+        return fichasPozo;
+    }
+
+    public void setFichasPozo(List<FichaDTO> fichasPozo) {
+        this.fichasPozo = fichasPozo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pozo{" + "fichasPozo=" + fichasPozo + '}';
+    }
+
 }
