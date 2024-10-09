fac4909feb69d003ec8e3d6a68fb2302211f4fe2
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-09 02:54:46 -0700

    Cambios minimos

--- DominoNegocio/src/main/java/DTOs/JugadorDTO.java
+++ DominoNegocio/src/main/java/DTOs/JugadorDTO.java
@@ -4,10 +4,75 @@
  */
 package DTOs;
 
+import Dominio.Ficha;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author INEGI
  */
 public class JugadorDTO {
-    
+     private String nombre;
+    private AvatarDTO avatar;
+    private String estado; //para cuando dicida abandonar partida osea si esta sale activo sino inactivo
+    private int puntuacion;
+    private List<Ficha> fichasJugador = new ArrayList<>();
+
+    public JugadorDTO() {
+        fichasJugador = new ArrayList<>();
+    }
+
+    public JugadorDTO(String nombre, AvatarDTO avatar, String estado, int puntuacion, List<Ficha> fichasJugador) {
+        this.nombre = nombre;
+        this.avatar = avatar;
+        this.estado = estado;
+        this.puntuacion = puntuacion;
+        this.fichasJugador = fichasJugador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public AvatarDTO getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(AvatarDTO avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public List<Ficha> getFichasJugador() {
+        return fichasJugador;
+    }
+
+    public void setFichasJugador(List<Ficha> fichasJugador) {
+        this.fichasJugador = fichasJugador;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + ", avatar=" + avatar + ", estado=" + estado + ", puntuacion=" + puntuacion + ", fichasJugador=" + fichasJugador + '}';
+    }
 }
