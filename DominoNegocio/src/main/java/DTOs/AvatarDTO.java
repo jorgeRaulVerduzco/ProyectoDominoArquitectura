8e59821d5f9ec202c0200fd96209194e0aa9d423
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-09 02:51:18 -0700

    sera una larga noche

--- DominoNegocio/src/main/java/DTOs/AvatarDTO.java
+++ DominoNegocio/src/main/java/DTOs/AvatarDTO.java
@@ -9,5 +9,26 @@ package DTOs;
  * @author INEGI
  */
 public class AvatarDTO {
-    
+    private String urlAvatar;
+
+    public AvatarDTO() {
+    }
+
+    public AvatarDTO(String urlAvatar) {
+        this.urlAvatar = urlAvatar;
+    }
+
+    public String getUrlAvatar() {
+        return urlAvatar;
+    }
+
+    public void setUrlAvatar(String urlAvatar) {
+        this.urlAvatar = urlAvatar;
+    }
+
+    @Override
+    public String toString() {
+        return "Avatar{" + "urlAvatar=" + urlAvatar + '}';
+    }
+
 }
