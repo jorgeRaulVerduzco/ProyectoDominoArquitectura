d54a2288dd38e466d2ab215da13aa678d2649a8f
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-10-11 04:02:24 -0700

    el juego corre

--- DominoNegocio/src/main/java/Main/Juego.java
+++ DominoNegocio/src/main/java/Main/Juego.java
@@ -7,9 +7,12 @@ package Main;
 import Presenctacion.Mediador;
 import Presenctacion.MenuPrincipalMVC.CrearUsuarioController;
 import Presenctacion.MenuPrincipalMVC.CrearUsuarioView;
+import Presenctacion.PozoMVC.PozoModel;
+import Presenctacion.PozoMVC.PozoView;
 import PresentacionTableroMVC.TableroController;
 import PresentacionTableroMVC.TableroModel;
 import PresentacionTableroMVC.TableroView;
+import java.awt.Frame;
 
 /**
  *
@@ -20,11 +23,11 @@ public class Juego {
     /**
      * @param args the command line arguments
      */
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         // Configurar el look and feel de la aplicación
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName()) ){
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -42,23 +45,26 @@ public class Juego {
     }
 
     private static void initializeApplication() {
-        // Crear las vistas
-      CrearUsuarioView crearUsuarioView = new CrearUsuarioView();
-    TableroView tableroView = new TableroView();
+        PozoModel pozoModel = new PozoModel(); // Crear un modelo de pozo
+        pozoModel.getPozo().inicializarFichas();
+        PozoView pozoView = new PozoView(new Frame(), true, pozoModel);
+            pozoView.setVisible(false); // Mantener el pozo invisible
+        CrearUsuarioView crearUsuarioView = new CrearUsuarioView();
+           TableroModel tableroModel = new TableroModel(); // Crear un modelo de tablero
+            TableroView tableroView = new TableroView(new Frame(), true, tableroModel, pozoModel);
 
-    // Crear los controladores
-    CrearUsuarioController crearUsuarioController = new CrearUsuarioController(crearUsuarioView);
-    TableroModel tableroModel = new TableroModel();
-    TableroController tableroController = new TableroController(tableroModel, tableroView);
+        // Crear los controladores
+        CrearUsuarioController crearUsuarioController = new CrearUsuarioController(crearUsuarioView);
+        TableroController tableroController = new TableroController(tableroModel, tableroView);
 
-    // Crear el mediador y pasarle los controladores y la vista del tablero
-    Mediador mediador = new Mediador(crearUsuarioController, tableroController, tableroView);
+        // Crear el mediador y pasarle los controladores y la vista del tablero
+        Mediador mediador = new Mediador(crearUsuarioController, tableroController, tableroView);
 
-    // Configurar los controladores para que usen el mediador
-    crearUsuarioController.setMediator(mediador);
-    tableroController.setMediator(mediador);
+        // Configurar los controladores para que usen el mediador
+        crearUsuarioController.setMediator(mediador);
+        tableroController.setMediator(mediador);
 
-    // Mostrar la vista inicial (en este caso, CrearUsuarioView)
-    mediador.iniciarAplicacion();
+        // Mostrar la vista inicial (en este caso, CrearUsuarioView)
+        mediador.iniciarAplicacion();
     }
 }
