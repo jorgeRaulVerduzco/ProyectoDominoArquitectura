05eb9d6c10004c5a11dbd62b7d61ba3d277354ed
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-11-10 04:24:15 -0700

    model
    mvc completo

--- DominoPresentacion/src/main/java/Presenctacion/CrearSalaMVC/CrearSalaModel.java
+++ DominoPresentacion/src/main/java/Presenctacion/CrearSalaMVC/CrearSalaModel.java
@@ -6,14 +6,65 @@ package Presenctacion.CrearSalaMVC;
 
 import Dominio.Sala;
 import Negocio.ServicioControlJuego;
+import Presenctacion.Observer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Observer;
 
 /**
  *
  * @author INEGI
  */
 public class CrearSalaModel {
-    
+
+    private int numeroJugadores;
+    private int numeroFichas;
+    private ServicioControlJuego servicioControlJuego;
+    private List<Observer> observers;
+
+    public CrearSalaModel() {
+        this.servicioControlJuego = new ServicioControlJuego();
+        this.observers = new ArrayList<>();
+    }
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+    public void crearSala() {
+        Sala nuevaSala = new Sala();
+        nuevaSala.setCantJugadores(numeroJugadores);
+        nuevaSala.setNumeroFichas(numeroFichas);
+        nuevaSala.setEstado("ESPERANDO");
+        servicioControlJuego.getSalasDisponibles().add(nuevaSala);
+        notifyObservers();
+    }
+
+    // Getters y Setters
+    public void setNumeroJugadores(int numeroJugadores) {
+        this.numeroJugadores = numeroJugadores;
+        notifyObservers();
+    }
+
+    public void setNumeroFichas(int numeroFichas) {
+        this.numeroFichas = numeroFichas;
+        notifyObservers();
+    }
+
+    public int getNumeroJugadores() {
+        return numeroJugadores;
+    }
+
+    public int getNumeroFichas() {
+        return numeroFichas;
+    }
 }
