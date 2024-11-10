46e7cf1a865d704c89d7bd59398f6fd12a795bec
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-11-10 04:23:26 -0700

    controller

--- DominoPresentacion/src/main/java/Presenctacion/CrearSalaMVC/CrearSalaController.java
+++ DominoPresentacion/src/main/java/Presenctacion/CrearSalaMVC/CrearSalaController.java
@@ -13,5 +13,68 @@ import javax.swing.JOptionPane;
  * @author INEGI
  */
 public class CrearSalaController {
+     private CrearSalaModel model;
+    private CrearSalaView view;
     
+    public CrearSalaController(CrearSalaModel model, CrearSalaView view) {
+        this.model = model;
+        this.view = view;
+        
+        // Inicializar listeners
+        this.view.addCrearSalaListener(new CrearSalaListener());
+        this.view.addRegresarListener(new RegresarListener());
+        this.view.addNumeroJugadoresListener(new NumeroJugadoresListener());
+        this.view.addNumeroFichasListener(new NumeroFichasListener());
+    }
+    
+    class CrearSalaListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                int numFichas = Integer.parseInt(view.getNumeroFichas());
+                if (numFichas <= 0) {
+                    JOptionPane.showMessageDialog(view, "El número de fichas debe ser positivo");
+                    return;
+                }
+                model.setNumeroFichas(numFichas);
+                model.setNumeroJugadores(Integer.parseInt(view.getNumeroJugadores()));
+                model.crearSala();
+                JOptionPane.showMessageDialog(view, "Sala creada exitosamente");
+                view.dispose(); // Cerrar ventana actual
+                // Aquí puedes abrir la siguiente ventana del flujo
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(view, "Por favor ingrese números válidos");
+            }
+        }
+    }
+    
+    class RegresarListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.dispose();
+        }
+    }
+    
+    class NumeroJugadoresListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                model.setNumeroJugadores(Integer.parseInt(view.getNumeroJugadores()));
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(view, "Número de jugadores inválido");
+            }
+        }
+    }
+    
+    class NumeroFichasListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                int numFichas = Integer.parseInt(view.getNumeroFichas());
+                model.setNumeroFichas(numFichas);
+            } catch (NumberFormatException ex) {
+                // El manejo se hace en el botón crear
+            }
+        }
+    }
 }
